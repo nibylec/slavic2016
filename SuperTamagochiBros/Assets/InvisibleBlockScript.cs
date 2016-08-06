9a064963657919ef72266fa81dc3c4134f523863
Witek Gawlowski <witek.gawlowski@gmail.com>
2016-08-06 09:18:04 +0200

    pierwsza większa wersja

---
+++ SuperTamagochiBros/Assets/InvisibleBlockScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvisibleBlockScript : MonoBehaviour {
+    Animator anim;
+    SpriteRenderer sr;
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    public void Enable()
+    {
+        sr.enabled = true;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.y < 0)
+        {
+            anim.SetTrigger("Destroy");
+        }
+    }
+}
