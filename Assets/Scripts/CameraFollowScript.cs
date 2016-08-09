ccdbfd9df48ba50460d254b1db8b9a57b518f0c9
Witek Gawlowski <witek.gawlowski@gmail.com>
2016-08-09 13:37:41 +0200

    usunięte przyklejanie się do ścian

--- Assets/Scripts/CameraFollowScript.cs
+++ Assets/Scripts/CameraFollowScript.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class CameraFollowScript : MonoBehaviour {
     // Update is called once per frame
     public GameObject objToFollow;
+    public float verticalShift;
 	void Update () {
         Vector3 objPosition = objToFollow.transform.position;
-        transform.position = new Vector3(objPosition.x, objPosition.y+0.5f, -10);
+        transform.position = new Vector3(objPosition.x, objPosition.y+verticalShift, -10);
 	}
 }
