66c965d9ce7e02025a12c25f4fc24fccd62c0d40
Witek Gawlowski <witek.gawlowski@gmail.com>
2016-08-12 16:13:44 +0200

    dodane checkpointy, poprawione tarcie na reszcie tilów

---
+++ Assets/CheckPointManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointManagerScript : MonoBehaviour {
+    public GameObject[] checkpoints;
+    public GameObject hero;
+    [HideInInspector]
+   
+
+    public void Start()
+    {
+        DeathPlaneScript.OnDeath += MoveHeroToLastCheckpoint;        
+    }
+
+    void Restart()
+    {
+        Application.LoadLevel(0);
+    }
+    void MoveHeroToLastCheckpoint()
+    {
+        for(int i = checkpoints.Length - 1; i>=0; i--)
+        {
+            if (checkpoints[i].GetComponent<CheckPointScript>().visited)
+            {
+                hero.transform.position = checkpoints[i].transform.position;
+                return;
+            }
+        }
+    }
+}
---
+++ Assets/CheckPointScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckPointScript : MonoBehaviour {
+    public bool visited;
+    public void Start()
+    {
+        visited = false;
+    }
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag != "Player")
+            return;
+        visited = true;
+    }
+}
